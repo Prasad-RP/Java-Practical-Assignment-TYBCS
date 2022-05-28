db675b74f147f99754968bed30b604ec8f18eafd
Pansare Prasad Rohidas <92712336+Prasad-RP@users.noreply.github.com>
2022-05-28 21:23:10 +0530

    Add files via upload

---
+++ SEM2 Assignment 4/Hello.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Hello extends HttpServlet{
+
+public void doGet(HttpServletRequest request,HttpServletResponse responce) throws IOException, ServletException
+{
+
+responce.setContentType("text/html");
+PrintWriter out= responce.getWriter();
+out.println("Hello Servelts");
+out.close();
+}
+
+}
+
